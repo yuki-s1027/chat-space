6672f650e23aa168bab49094a7a9cbac8db4b7b5
yuki-s1027 <yuki.109921@gmail.com>
2020-01-20 14:17:52 +0900

    不要箇所の消去
    不要箇所の消去

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,6 @@ $(function(){
 
   var reloadMessages = function() {
     last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',
