86c4e0505e22e8460dc8f4dbd8c66005cf60fb01
yuki-s1027 <yuki.109921@gmail.com>
2020-01-21 18:33:48 +0900

    remove default env

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,9 +48,9 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-set :default_env, {
-  rbenv_root: "usr/local/rbenv",
-  path: "usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+# set :default_env, {
+#   rbenv_root: "usr/local/rbenv",
+#   path: "usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# }
